2d6028cd01e2a699d8b047b5889aa55345b7ca76
Mats Stijlaart <m.stijlaart@avisi.nl>
2016-05-09 21:24:40 +0200

    1.0.2

--- src/main/java/nl/stil4m/elm/ElmMakeMojo.java
+++ src/main/java/nl/stil4m/elm/ElmMakeMojo.java
@@ -54,7 +54,7 @@ public class ElmMakeMojo extends AbstractMojo {
 
         try {
             Runtime rt = Runtime.getRuntime();
-            Process pr = new ProcessBuilder(command).directory(project.getBasedir()).start();
+            Process pr = rt.exec(command, new String[]{}, project.getBasedir());
 
             int exitValue = pr.waitFor();
             if (exitValue != 0) {
